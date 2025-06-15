682b57227a2966fdd28fe3a266283e89b3ad9630
sight011 <kfcsport@googlemail.com>
2025-06-15 20:11:08 +0200

    Fixes + new Header Design – Stable Version

--- frontend/src/components/TaskBoard/TaskBoard.jsx
+++ frontend/src/components/TaskBoard/TaskBoard.jsx
@@ -396,7 +396,7 @@ const TaskBoard = () => {
             datasets: [{
                 data: [
                     counts.todo || 0,
-                    counts.in_progress || 0,
+                    counts.inProgress || 0,
                     counts.review || 0,
                     counts.done || 0
                 ],
@@ -522,15 +522,6 @@ const TaskBoard = () => {
                             <path d="M4 20h16v-2H4v2zm0-4h10v-2H4v2zm0-4h7V6h3l-4-4-4 4h3v6z" fill="currentColor"/>
                         </svg>
                         <span>Burn Down</span>
-                    </button>
-                    <button
-                        className="settings-button"
-                        onClick={() => {/* TODO: Navigate to settings page */}}
-                        title="Settings"
-                    >
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z" fill="currentColor"/>
-                        </svg>
                     </button>
                 </div>
             </div>
